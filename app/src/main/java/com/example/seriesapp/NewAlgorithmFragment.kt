743cacdb8f1114a15fc26bf00d35ee4fd0533b39
alejandro <alejandromontero1551@gmail.com>
2023-04-03 10:59:02 -0400

    updates saveAlgo function

--- app/src/main/java/com/example/seriesapp/NewAlgorithmFragment.kt
+++ app/src/main/java/com/example/seriesapp/NewAlgorithmFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
@@ -34,16 +35,41 @@ class NewAlgorithmFragment : Fragment(), View.OnClickListener  {
 
         nameInput = view.findViewById(R.id.inputName)
         algorithmInput = view.findViewById(R.id.inputAlgorithm)
-        submitButton = view.findViewById<Button>(R.id.button)
+        submitButton = view.findViewById<Button>(R.id.submitButton)
         submitButton.setOnClickListener(this)
         db = FirebaseFirestore.getInstance()
 
         return view;
     }
 
-    override fun onClick(v: item){
+    override fun onClick(v: View?){
         when(v?.id){
-            
+            R.id.submitButton -> saveData()
         }
+    }
+
+    private fun saveData() {
+
+        val name = nameInput.text.toString()
+        val algorithm = algorithmInput.text.toString()
+
+        val data = hashMapOf(
+            "name" to name,
+            "algorithm" to algorithm
+        )
+
+        db.collection("algorithms")
+            .add(data)
+            .addOnSuccessListener { documentReference ->
+                    nameInput.setText("")
+                    algorithmInput.setText("")
+
+                    Toast.makeText(context, "Algorithm saved successfully!", Toast.LENGTH_SHORT).show()
+
+            }
+            .addOnFailureListener { e ->
+                // There was an error saving the data
+                Toast.makeText(context, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
